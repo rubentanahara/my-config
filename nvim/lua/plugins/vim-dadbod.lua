e015353445372ed2bd82650a47037c4a487b283c
rubentanahara <ruben.mtz.tanahara@outlook.com>
2024-12-28 12:28:29 -0800

    update: Adding Spotify and Unity to karabinder

--- nvim/lua/plugins/vim-dadbod.lua
+++ nvim/lua/plugins/vim-dadbod.lua
@@ -1,8 +1,34 @@
 return {
-  "tpope/vim-dadbod",
-  opt = true, -- optional plugin, mark as optional
-  requires = {
-    "kristijanhusak/vim-dadbod-ui",
-    "kristijanhusak/vim-dadbod-completion",
+  "kristijanhusak/vim-dadbod-ui",
+  dependencies = {
+    { "tpope/vim-dadbod", lazy = true },
+    { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
   },
+  cmd = {
+    "DBUI",
+    "DBUIToggle",
+    "DBUIAddConnection",
+    "DBUIFindBuffer",
+  },
+  init = function()
+    -- Enable nerd fonts
+    vim.g.db_ui_use_nerd_fonts = 1
+
+    -- Configure your database connections
+    vim.g.dbs = {
+      { name = "dev", url = "postgres://postgres:mypassword@localhost:5432/my-dev-db" },
+      { name = "staging", url = "postgres://postgres:mypassword@localhost:5432/my-staging-db" },
+      { name = "wp", url = "mysql://root@localhost/wp_awesome" },
+      {
+        name = "production",
+        url = function()
+          return vim.fn.system("get-prod-url")
+        end,
+      },
+    }
+
+    -- Optional: Add additional settings
+    vim.g.db_ui_save_location = vim.fn.stdpath("data") .. "/db_ui"
+    vim.g.db_ui_auto_execute_table_helpers = 1
+  end,
 }
