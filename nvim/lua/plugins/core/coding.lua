9cb2eb2614b949897fdcb90e7594be70e4447847
rubentanahara <ruben.mtz.tanahara@outlook.com>
2025-03-16 07:52:25 -0700

    update: util windows on tmux

--- nvim/lua/plugins/core/coding.lua
+++ nvim/lua/plugins/core/coding.lua
@@ -523,10 +523,11 @@ return {
     version = false,
     event = 'InsertEnter',
     dependencies = {
-      'hrsh7th/cmp-nvim-lsp',
-      'hrsh7th/cmp-buffer',
-      'hrsh7th/cmp-path',
-      'saadparwaiz1/cmp_luasnip',
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-cmdline",
+      "saadparwaiz1/cmp_luasnip",
       {
         'L3MON4D3/LuaSnip',
         build = (not jit.os:find 'Windows') and
--- nvim/lua/plugins/core/util.lua
+++ nvim/lua/plugins/core/util.lua
@@ -67,9 +67,11 @@ return {
   {
     "iamcco/markdown-preview.nvim",
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    build = function()
-      vim.fn["mkdp#util#install"]()
+    build = "cd app && yarn install",
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
     end,
+    ft = { "markdown" },
     keys = {
       { "<leader>mp", "<cmd>MarkdownPreviewToggle<cr>", desc = "Markdown Preview" },
     },
